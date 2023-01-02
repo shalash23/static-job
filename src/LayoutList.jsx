04a322eb045b5176fc6bfd73395774881d9941bb
shalash23 <shalashmohamed232@outlook.com>
2023-01-02 15:36:47 +0200

    starting responsive layout

--- src/LayoutList.jsx
+++ src/LayoutList.jsx
@@ -2,30 +2,56 @@ import React from "react";
 import Card from "./components/Layouts/Card";
 import images from "./assets/index";
 import data from "../../data.json";
+import { useMediaQuery, useTheme } from "@mui/material";
+import CardDesktop from "./components/Layouts/CardDesktop";
 
 const LayoutList = () => {
-  const cards = data.map((item) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
+  const cardMobile = data.map((item) => {
     const { logo } = item;
     return (
-      <Card
-        key={item.id}
-        name={item.company}
-        logo={images[logo]}
-        newItem={item.new}
-        featured={item.featured}
-        position={item.position}
-        role={item.role}
-        level={item.level}
-        postedAt={item.postedAt}
-        contract={item.contract}
-        location={item.location}
-        languages={item.languages}
-        tools={item.tools}
-      />
+      <React.Fragment key={item.id}>
+        <Card
+          name={item.company}
+          logo={images[logo]}
+          newItem={item.new}
+          featured={item.featured}
+          position={item.position}
+          role={item.role}
+          level={item.level}
+          postedAt={item.postedAt}
+          contract={item.contract}
+          location={item.location}
+          languages={item.languages}
+          tools={item.tools}
+        />
+      </React.Fragment>
+    );
+  });
+  const cardDesktop = data.map((item) => {
+    const { logo } = item;
+    return (
+      <React.Fragment key={item.id}>
+        <CardDesktop
+          name={item.company}
+          logo={images[logo]}
+          newItem={item.new}
+          featured={item.featured}
+          position={item.position}
+          role={item.role}
+          level={item.level}
+          postedAt={item.postedAt}
+          contract={item.contract}
+          location={item.location}
+          languages={item.languages}
+          tools={item.tools}
+        />
+      </React.Fragment>
     );
   });
 
-  return <>{cards}</>;
+  return <>{matches ? cardMobile : cardDesktop}</>;
 };
 
 export default LayoutList;
